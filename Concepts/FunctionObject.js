4abadcb335b32129db52ca6c510c92fc0c2bdeb9
Jitesh Singh <jiteshs101@gmail.com>
2026-01-05 19:48:31 +0530

    Function is actual an object in js

---
+++ Concepts/FunctionObject.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log("Inbuilt Property Name :: ", add.name)
+console.log("Inbuilt Property Length(No of Arguments) :: ", add.length)
+
+console.log("Inbuilt Behaviour :: ", add.call)
+console.log("Inbuilt Behaviour :: ", add.apply)
+console.log("Inbuilt Behaviour :: ", add.bind)
