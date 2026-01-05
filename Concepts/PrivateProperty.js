23b7336630ea01e308e5bd2426375a6e51c0243a
Jitesh Singh <jiteshs101@gmail.com>
2026-01-05 19:50:37 +0530

    Priavte Variable in JS

---
+++ Concepts/PrivateProperty.js
@@ -0,0 +1,33 @@
+function createBankAccount(initialBalance) {
+    let balance = initialBalance;  // PRIVATE - can't be accessed directly
+
+    return {
+        deposit: function (amount) {
+            balance += amount;
+            return balance;
+        },
+
+        withdraw: function (amount) {
+            if (amount > balance) {
+                return "Insufficient funds";
+            }
+            balance -= amount;
+            return balance;
+        },
+
+        getBalance: function () {
+            return balance;
+        }
+    };
+}
+
+const myAccount = createBankAccount(100);
+
+console.log(myAccount.getBalance());  // 100
+myAccount.deposit(50);  // 150
+myAccount.withdraw(30);  // 120
+
+// Can't directly access or modify balance!
+console.log(myAccount.balance);  // undefined
+myAccount.balance = 9999999;  // Doesn't work!
+console.log(myAccount.getBalance());  // 120 - still protected
