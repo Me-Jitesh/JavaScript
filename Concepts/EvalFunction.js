e441a83b280edd1fb8a38d3a480b85039135155a
Jitesh Singh <jiteshs101@gmail.com>
2026-04-25 20:27:36 +0530

    Eval Function

---
+++ Concepts/EvalFunction.js
@@ -0,0 +1,10 @@
+// eval() is built-in function  that evaluates a string as a JavaScript code and dynamically execute it
+
+let x = 10;
+let y = 20;
+
+let exp = "x+y";
+
+let res = eval(exp);
+
+console.log(res);
